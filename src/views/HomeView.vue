923ecb60899e5a33e9fc2d7c7623e542ca616056
jlovrek1 <josephlovrekovic@gmail.com>
2024-03-12 11:58:33 -0400

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HomeScreen from '@/components/HomeScreen.vue';
+import TheWelcome from '../components/TheWelcome.vue';
+</script>
+
+<template>
+  <main>
+    <HomeScreen />
+  </main>
+</template>

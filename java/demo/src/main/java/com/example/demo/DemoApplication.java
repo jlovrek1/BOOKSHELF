22a5006e1c5196a0dc738f99fef79cfe4c8c769a
jlovrek1 <josephlovrekovic@gmail.com>
2024-04-04 09:36:54 -0400

    lets gooooo

--- java/demo/src/main/java/com/example/demo/DemoApplication.java
+++ java/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,14 @@
-package com.example.bookshelf;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookshelfApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookshelfApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
+
